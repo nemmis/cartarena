68689d292815f747fa6ab3064b3c36ca31cba665
nemmis <nemmis@users.noreply.github.com>
2017-01-12 20:43:01 +0100

    Assign a gamepad to a player. Exit the game when the back button is pressed.

--- gamepad/main.lua
+++ gamepad/main.lua
@@ -1,36 +1,72 @@
 -- Test for gamepads
--- See when joystick are connected / disconnected using callbacks
--- When a joystick is disconnected then reconnected the id remains stable
+-- See when joysticks are connected / disconnected using callbacks
+-- When a joystick is disconnected then reconnected its id remains stable
+-- The player has one gamepad
+-- The last button pressed on any gamepad is displayed
+-- Can exit the game using the back button of the controller
+
+local lastButtonPressed = "none"
+local player = { hasGamepad = false, gamepad = nil}
 
 function love.load()
-  joystickCount = love.joystick.getJoystickCount()
+  local joystickCount = love.joystick.getJoystickCount()
   print("There are " .. joystickCount .. " joysticks connected at game launch")
 
-  joysticks = love.joystick.getJoysticks()
+  local joysticks = love.joystick.getJoysticks()
   for i, joystick in ipairs(joysticks)
   do
     print('Joystick' .. i .. ': ' .. toStringJoystick(joystick))
   end
 
+-- see gamepad mappings
+  local mappings = love.joystick.saveGamepadMappings( )
+  print("Gamepad mappings: " .. mappings)
   print('love.load finished')
 end
 
 function love.update(dt)
 
+  -- exit if back is pressed
+  if player.hasGamepad and player.gamepad:isGamepadDown('back')
+  then love.event.quit()
+  end
+
 end
 
-function love.render()
-
+function love.draw()
+  -- display the last button pressed
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.print("Last button of the gamepad pressed: " .. lastButtonPressed, 5, 5)
 end
 
 -- callbacks
--- can be used to handle disconnection and
+-- can be used to handle disconnection
+-- called after love.load
 function love.joystickadded(joystick)
   print('Joystick added: ' .. toStringJoystick(joystick))
+  -- add the joystick to the player if needed
+  if joystick:isGamepad() and not player.hasGamepad
+  then player.hasGamepad = true
+    player.gamepad = joystick
+    print("Gamepad assigned to the player")
+  end
+
 end
 
 function love.joystickremoved(joystick)
   print('Joystick removed: ' .. toStringJoystick(joystick))
+
+  -- check if the joystick from the player was removed
+  if player.gamepad:getID() == joystick:getID()
+    then player.gamepad = nil
+      player.hasGamepad = false
+      print("Gamepad removed from player")
+    end
+end
+
+function love.gamepadpressed(joystick, button)
+  print("Last button pressed by any gamepad: " .. button)
+  lastButtonPressed = button
 end
 
 -- utils
