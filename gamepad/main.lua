2224aad1c6f4b72da4b57842e44f7c9eef72413b
nemmis <nemmis@users.noreply.github.com>
2017-01-13 18:37:13 +0100

    Render the state of the gamepad's right thumbstick in the game window

--- gamepad/main.lua
+++ gamepad/main.lua
@@ -7,8 +7,13 @@
 
 local lastButtonPressed = "none"
 local player = { hasGamepad = false, gamepad = nil}
+local rightThumbstick = { xAxis = 0, yAxis = 0}
 
 function love.load()
+  local major, minor, revision, codename = love.getVersion()
+  local str = string.format("%d.%d.%d - %s", major, minor, revision, codename)
+  print("Running LOVE version " .. str)
+
   local joystickCount = love.joystick.getJoystickCount()
   print("There are " .. joystickCount .. " joysticks connected at game launch")
 
@@ -26,17 +31,28 @@ end
 
 function love.update(dt)
 
-  -- exit if back is pressed
-  if player.hasGamepad and player.gamepad:isGamepadDown('back')
-  then love.event.quit()
-  end
+  if player.hasGamepad
+  then
 
+    -- exit if back is pressed
+     if player.gamepad:isGamepadDown('back')
+     then love.event.quit()
+     end
+
+     -- get the state of the right thumbstick
+     rightThumbstick.xAxis = player.gamepad:getGamepadAxis("rightx")
+     rightThumbstick.yAxis = player.gamepad:getGamepadAxis("righty")
+ end
+
+  -- get the status
 end
 
 function love.draw()
   -- display the last button pressed
   love.graphics.setColor(255, 255, 255)
   love.graphics.print("Last button of the gamepad pressed: " .. lastButtonPressed, 5, 5)
+
+  renderThumbstick(rightThumbstick)
 end
 
 -- callbacks
@@ -66,10 +82,17 @@ end
 
 function love.gamepadpressed(joystick, button)
   print("Last button pressed by any gamepad: " .. button)
+  print("Right thumbstick values: " .. rightThumbstick.xAxis .. " - " .. rightThumbstick.yAxis)
   lastButtonPressed = button
 end
 
 -- utils
 function toStringJoystick(joystick)
   return "name(" .. joystick:getName() .. "), isGamepad(" .. tostring(joystick:isGamepad()) .. "), stable_id(" .. joystick:getID() .. "), GUID(" .. joystick:getGUID() .. ")"
+end
+
+function renderThumbstick(thumbstick)
+  local scale = 50
+  love.graphics.circle("fill", love.graphics.getWidth() / 2 + thumbstick.xAxis * scale, love.graphics.getHeight() / 2 + thumbstick.yAxis * scale, 2)
+  love.graphics.circle("line", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, scale)
 end
