2f96256beeb4c3c172efd4581fcf354be894ab8e
nemmis <nemmis@users.noreply.github.com>
2017-01-29 18:34:48 +0100

    Add a second player

--- main.lua
+++ main.lua
@@ -15,22 +15,34 @@ local debuggingEnabled = true
 local HEIGHT = love.graphics.getHeight()
 local WIDTH = love.graphics.getWidth()
 
+-- player can be gamepad + vehicle + trajectory
 local drivingInputType = vehicleInput.TYPE_THIRD_PERSON()
 local gamepad
+local gamepad2
 local vehicle
+local vehicle2
 local map
 local trajectory
+local trajectory2
 
 local rectangleObstacle
 
 function love.load()
+  -- input
   gamepad = love.joystick.getJoysticks()[1]
+  gamepad2 = love.joystick.getJoysticks()[2]
 
+  -- map
   mapModule.init(HC)
 
+  -- vehicles
   vehicleModule.init(HC)
   vehicle = vehicleModule.new(WIDTH / 16, HEIGHT / 8, 0, debuggingEnabled)
+  vehicle2 = vehicleModule.new(4 * WIDTH / 16, HEIGHT / 8, 0, debuggingEnabled)
+
+  -- trajectories
   trajectory = trajectoryModule.new()
+  trajectory2 = trajectoryModule.new()
 end
 
 function love.update(dt)
@@ -40,13 +52,19 @@ function love.update(dt)
 		then love.event.quit()
 	end
 
-  -- driving inputs
-  local accelerates, breaks, steers = vehicleInput.getDriverInput(gamepad, vehicle, drivingInputType)
+  -- first player
+  if gamepad then
+    local accelerates, breaks, steers = vehicleInput.getDriverInput(gamepad, vehicle, drivingInputType)
+    vehicle:update(dt, accelerates, breaks, steers)
+    trajectory:add(vehicle.x, vehicle.y)
+  end
 
-  vehicle:update(dt, accelerates, breaks, steers)
-
-  trajectory:add(vehicle.x, vehicle.y)
-
+  -- second player
+  if gamepad2 then
+    local accelerates, breaks, steers = vehicleInput.getDriverInput(gamepad2, vehicle2, drivingInputType)
+    vehicle2:update(dt, accelerates, breaks, steers)
+    trajectory2:add(vehicle2.x, vehicle2.y)
+  end
 end
 
 function love.draw()
@@ -54,10 +72,15 @@ function love.draw()
   love.graphics.setColor(colors.WHITE())
   love.graphics.print("FPS: " .. love.timer.getFPS(), 5, 5)
 
-  vehicle:draw()
+
   mapModule.draw()
+
+  vehicle:draw()
   trajectory:draw()
-  
+
+  vehicle2:draw()
+  trajectory2:draw()
+
   if debuggingEnabled
       then
       -- driving mode
@@ -67,10 +90,13 @@ function love.draw()
 end
 
 function love.gamepadpressed( joystick, button )
+  -- any gamepad
+
   -- toggle debug drawing
   if button == 'start'
   then debuggingEnabled = not debuggingEnabled
     vehicle:setDebug(debuggingEnabled)
+    vehicle2:setDebug(debuggingEnabled)
   end
 
   if button == 'y'
