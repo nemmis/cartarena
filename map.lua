1c6d72a668b31a3b9133e027ded44d6e3d7c9532
nemmis <nemmis@users.noreply.github.com>
2017-01-29 18:45:48 +0100

    Add boundaries to the map

--- map.lua
+++ map.lua
@@ -9,8 +9,13 @@ local WIDTH = love.graphics.getWidth()
 function mapModule.init(collisionEngine)
   mapModule.collisionEngine = collisionEngine
 
-  -- shapes are stored directly as a HC shape, TODO change this later
+  -- enforce map boundaries
+  mapModule.up = mapModule.collisionEngine.rectangle(0, 0, WIDTH, HEIGHT / 100)
+  mapModule.down = mapModule.collisionEngine.rectangle(0, HEIGHT * 99 / 100, WIDTH, HEIGHT / 100)
+  mapModule.left = mapModule.collisionEngine.rectangle(0, 0, WIDTH / 200, HEIGHT)
+  mapModule.right = mapModule.collisionEngine.rectangle(WIDTH * 199 / 200, 0, WIDTH / 200, HEIGHT)
 
+  -- shapes are stored directly as a HC shape, TODO change this later
   mapModule.rectangle = {
     ax = WIDTH/4,
     ay = HEIGHT/2,
@@ -43,6 +48,11 @@ function mapModule.draw()
 
   love.graphics.setColor(colors.TURQUESE())
 
+  -- boundaries
+  mapModule.up:draw()
+  mapModule.down:draw()
+  mapModule.right:draw()
+  mapModule.left:draw()
   mapModule.rectangle.bbColl:draw()
   mapModule.rectangle2.bbColl:draw()
   mapModule.circle.bbColl:draw()
